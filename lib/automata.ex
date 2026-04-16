836b55dbdd6c0251faab05a24d1b7d1607d08b0f
AlguienSasaki <A01739611@tec.mx>
2026-04-15 22:47:57 -0600

    determinze function + test cases

--- lib/automata.ex
+++ lib/automata.ex
@@ -1,18 +1,45 @@
 defmodule Automata do
-  @moduledoc """
-  Documentation for `Automata`.
-  """
-
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Automata.hello()
-      :world
-
+  Implement the determinize/1 function, which receives the input non-deterministic finite automaton as a parameter and returns the newly calculated deterministic finite automaton. You are free to choose the representation you consider most appropriate for the automaton (e.g., map, tuple, etc.).
   """
-  def hello do
-    :world
+  def determinize({_q, sigma, delta, q0, f}) do
+    initial_state = MapSet.new([q0]) 
+    
+    {new_delta, all_states} = build_dfa_transitions([initial_state], MapSet.new([initial_state]), %{}, delta, sigma)
+    
+    new_q = MapSet.to_list(all_states)
+    
+    final_states = Enum.filter(new_q, fn r -> 
+      not MapSet.disjoint?(r, MapSet.new(f)) 
+    end) 
+
+    {new_q, sigma, new_delta, initial_state, final_states}
+  end
+
+  defp build_dfa_transitions([], all_states, dfa_delta, _nfa_delta, _sigma), do: {dfa_delta, all_states}
+
+  defp build_dfa_transitions([current_r | rest], all_states, dfa_delta, nfa_delta, sigma) do
+    {updated_delta, new_to_explore, updated_all_states} = 
+      Enum.reduce(sigma, {dfa_delta, [], all_states}, fn symbol, {acc_delta, acc_explore, acc_all} ->
+        
+        next_r = 
+          current_r
+          |> Enum.flat_map(fn q -> Map.get(nfa_delta, {q, symbol}, []) end)
+          |> MapSet.new()
+
+        if MapSet.size(next_r) > 0 do
+          new_acc_delta = Map.put(acc_delta, {current_r, symbol}, next_r)
+          
+          if MapSet.member?(acc_all, next_r) do
+            {new_acc_delta, acc_explore, acc_all}
+          else
+            {new_acc_delta, [next_r | acc_explore], MapSet.put(acc_all, next_r)}
+          end
+        else
+          {acc_delta, acc_explore, acc_all}
+        end
+      end)
+
+    build_dfa_transitions(rest ++ new_to_explore, updated_all_states, updated_delta, nfa_delta, sigma)
   end
 end
--- test/automata_test.exs
+++ test/automata_test.exs
@@ -1,8 +1,25 @@
 defmodule AutomataTest do
   use ExUnit.Case
-  doctest Automata
+  alias Automata 
 
-  test "greets the world" do
-    assert Automata.hello() == :world
+  test "determinización del autómata de la Parte 1 (Página 2)" do
+    nfa_delta = %{
+      {0, "a"} => [0, 1],
+      {0, "b"} => [0],
+      {1, "b"} => [2],
+      {2, "b"} => [3]
+    }
+    nfa = {[0, 1, 2, 3], ["a", "b"], nfa_delta, 0, [3]}
+
+    dfa = Automata.determinize(nfa)
+    {_q_prime, _sigma, delta_prime, start_prime, f_prime} = dfa
+
+    assert MapSet.equal?(start_prime, MapSet.new([0]))
+
+    assert MapSet.equal?(Map.get(delta_prime, {MapSet.new([0]), "a"}), MapSet.new([0, 1]))
+
+    for state <- f_prime do
+      assert MapSet.member?(state, 3)
+    end
   end
 end
